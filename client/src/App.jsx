3ee1dbad0b5d80d3ff531b5aa4ea9f2daeb407b0
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-13 18:00:40 -0500

    added navbar and icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,57 @@
 import './App.css'
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet/>
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
     children: [
       {
-        path: "/home", 
+        path: "/", 
         element: <Home/>
-      }
+      },
+      // {
+      //   path: "/tickets",
+      //   element: <Tickets />
+      // },
+      // {
+      //   path: "/attractions",
+      //   element: <Attractions />
+      // },
+      // {
+      //   path: "/shop",
+      //   element: <Shop />
+      // },
+      // {
+      //   path: "/signup",
+      //   element: <Signup />
+      // },
+      // {
+      //   path: "/login", 
+      //   element: <Login/>
+      // },
+      // {
+      //   path: "/signup",
+      //   element: <Signup />
+      // },
     ]
-  }
-])
+  },
+  {
+    path: "*",
+    element: <h1>Page Not Found</h1>
+  },
+]);
 
 function App() {
 
