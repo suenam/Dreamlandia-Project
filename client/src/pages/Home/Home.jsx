7564ae38e4294c4af76399b226fdf9b9fea86d07
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-13 16:45:22 -0500

    created homepage, added assets

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import './Home.css'
+import { Link } from "react-router-dom";
 
-function Home() {
-
+const Home = () => {
   return (
-    <>Home</>
-  )
+    <div className="home-container">
+      <div className="home-text">
+        <h1>Dreamlandia, the dreamiest <br /> place on Earth!</h1>
+        <p>
+        <Link className="link" to="/tickets">Buy Tickets</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home
