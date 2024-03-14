f949e165efc50a7483919a728fb31fd4e229cc31
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-14 15:48:46 -0500

    added material UI dependency and added tickets and attractions selections on ticket page

--- client/src/pages/Tickets/Tickets.jsx
+++ client/src/pages/Tickets/Tickets.jsx
@@ -1,4 +1,7 @@
 import './Tickets.css'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import Attraction1 from '../../assets/attraction_1.jpg';
 
 const Tickets = () => {
     return (
@@ -14,15 +17,62 @@ const Tickets = () => {
                         <div className='ticket-option'>
                             <h3>Standard</h3>
                             <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
+                            <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon />
+                                <p> 4 </p>
+                                <AddCircleOutlineIcon />
+                            </div>
                         </div>
                         <div className='ticket-option'>
-                            <h3>Standard</h3>
+                            <h3>Express</h3>
                             <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
                         </div>
                         <div className='ticket-option'>
-                            <h3>Standard</h3>
+                            <h3>Child</h3>
                             <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
                         </div>
+                    </div>
+                </div>
+
+                <div className='attractions-content'>
+                    <div className='select-attractions'>
+                        <h2>Select the attractions you're planning to ride</h2>
+                        <div className='attractions-options'>
+                            <div className='attraction-option'>
+                                <img src={Attraction1} alt="attraction1" />
+                                <div className='attraction-desc'>
+                                    <h3>Attraction 1</h3>
+                                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam delectus eius doloremque maiores veritatis!</div>
+                                </div>
+                            </div>
+                            <div className='attraction-option'>
+                                <img src={Attraction1} alt="attraction1" />
+                                <div className='attraction-desc'>
+                                    <h3>Attraction 1</h3>
+                                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam delectus eius doloremque maiores veritatis!</div>
+                                </div>
+                            </div>
+                            <div className='attraction-option'>
+                                <img src={Attraction1} alt="attraction1" />
+                                <div className='attraction-desc'>
+                                    <h3>Attraction 1</h3>
+                                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam delectus eius doloremque maiores veritatis!</div>
+                                </div>
+                            </div>
+                            <div className='attraction-option'>
+                                <img src={Attraction1} alt="attraction1" />
+                                <div className='attraction-desc'>
+                                    <h3>Attraction 1</h3>
+                                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam delectus eius doloremque maiores veritatis!</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='meal-content'>
+                    <div className='select-meals'>
+                        <h2>Select your meal vouchers</h2>  
                     </div>
                 </div>
             </div>
