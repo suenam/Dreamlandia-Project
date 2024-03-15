1c5a98e1a28c16f5866fd84943b7a30775b9bf3a
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-15 17:38:02 -0500

    added ticket prices, and toggle button for adding tickets, and added MUI dependency in client side

--- client/src/pages/Tickets/Tickets.jsx
+++ client/src/pages/Tickets/Tickets.jsx
@@ -1,4 +1,5 @@
 import './Tickets.css'
+import { useState } from 'react';
 import Attraction1 from '../../assets/attraction_1.jpg';
 import Food1 from '../../assets/whataburger.jpg';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -6,6 +7,15 @@ import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Tickets = () => {
+
+    const [standardTicket, setStandardTicket] = useState(0);
+    const [expressTicket, setExpressTicket] = useState(0);
+    const [childTicket, setChildTicket] = useState(0);
+
+    const [standardFoodTicket, setStandardFoodTicket] = useState(0);
+    const [expressFoodTicket, setExpressFoodTicket] = useState(0);
+    const [deluxeFoodTicket, setDeluxeFoodTicket] = useState(0);
+    
     return (
         <div className="tickets-container">
             <div className='tickets-header'>
@@ -18,20 +28,45 @@ const Tickets = () => {
                     <div className='ticket-options'>
                         <div className='ticket-option'>
                             <h3>Standard</h3>
-                            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
+                            <h4>$65</h4>
+                            <div>Provides full-day access to thrilling rides, entertaining shows, and unique attractions for guests of all ages</div>
                             <div className='remove-add-tickets'>
-                                <RemoveCircleOutlineIcon />
-                                <p> 4 </p>
-                                <AddCircleOutlineIcon />
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={standardTicket === 0? 'none': ''} color={standardTicket === 0? 'action' : ''} 
+                                    onClick={()=>setStandardTicket(standardTicket-1)}
+                                />
+                                <div className='ticket-count'>{standardTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setStandardTicket(standardTicket+1)}
+                                />
                             </div>
                         </div>
                         <div className='ticket-option'>
                             <h3>Express</h3>
-                            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
+                            <h4>$90</h4>
+                            <div>Offers priority access to select rides and attractions, letting you skip the lines and experience more in less time</div>
+                            <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={expressTicket === 0? 'none': ''} color={expressTicket === 0? 'action' : ''}
+                                    onClick={()=>setExpressTicket(expressTicket-1)}
+                                />
+                                <div className='ticket-count'>{expressTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setExpressTicket(expressTicket+1)}
+                                />
+                            </div>
                         </div>
                         <div className='ticket-option'>
                             <h3>Child</h3>
-                            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, modi!</div>
+                            <h4>$45</h4>
+                            <div>Offering kids aged 3-12 discounted access to a magical day of rides, shows, and attractions</div>
+                            <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={childTicket === 0? 'none': ''} color={childTicket === 0? 'action' : ''}
+                                    onClick={()=>setChildTicket(childTicket-1)}
+                                />
+                                <div className='ticket-count'>{childTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setChildTicket(childTicket+1)}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -52,6 +87,7 @@ const Tickets = () => {
                                 <div className='attraction-desc'>
                                     <h3>Attraction 1</h3>
                                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam delectus eius doloremque maiores veritatis!</div>
+
                                 </div>
                             </div>
                             <div className='attraction-option'>
@@ -80,16 +116,43 @@ const Tickets = () => {
                                 <img src={Food1} alt="Food1" />
                                 <h3> Whataburger </h3>
                                 <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, doloremque.</div>
+                                <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={standardFoodTicket === 0? 'none': ''} color={standardFoodTicket === 0? 'action' : ''}
+                                    onClick={()=>setStandardFoodTicket(standardFoodTicket-1)}
+                                />
+                                <div className='ticket-count'>{standardFoodTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setStandardFoodTicket(standardFoodTicket+1)}
+                                />
+                            </div>
                             </div>
                             <div className='meal-option'>
                                 <img src={Food1} alt="Food1" />
                                 <h3> Whataburger </h3>
                                 <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, doloremque.</div>
+                                <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={expressFoodTicket === 0? 'none': ''} color={expressFoodTicket === 0? 'action' : ''}
+                                    onClick={()=>setExpressFoodTicket(expressFoodTicket-1)}
+                                />
+                                <div className='ticket-count'>{expressFoodTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setExpressFoodTicket(expressFoodTicket+1)}
+                                />
+                            </div>
                             </div>
                             <div className='meal-option'>
                                 <img src={Food1} alt="Food1" />
                                 <h3> Whataburger </h3>
                                 <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, doloremque.</div>
+                                <div className='remove-add-tickets'>
+                                <RemoveCircleOutlineIcon fontSize='large' pointerEvents={deluxeFoodTicket === 0? 'none': ''} color={deluxeFoodTicket === 0? 'action' : ''}
+                                    onClick={()=>setDeluxeFoodTicket(deluxeFoodTicket-1)}
+                                />
+                                <div className='ticket-count'>{deluxeFoodTicket}</div>
+                                <AddCircleOutlineIcon fontSize='large' 
+                                    onClick={()=>setDeluxeFoodTicket(deluxeFoodTicket+1)}
+                                />
+                            </div>
                             </div>
                         </div>
                     </div>
