8cc50eca4ee01db777962b40092c1cd06a948702
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-20 17:18:23 -0500

    added handle checkout for tickets

---
+++ client/src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,17 @@
+
+const Checkout = () => {
+    return (
+        <>
+        <div className="checkout-container">
+            <div className='checkout-info'>
+                checkout info and input forms here  
+            </div>
+            <div className='item-summary'>
+                summary of items
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Checkout
