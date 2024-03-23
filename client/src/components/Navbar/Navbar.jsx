07928fcb88ab26e9bf0bfbbbd3eb671de5696b69
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-23 15:00:48 -0500

    logout only appears for users/customers

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                     <NavLink className="link" to='/attractions'>Attractions</NavLink>
 
                     <NavLink className="link" to='/shop'>Shop</NavLink>
-                    {!auth.user && !auth.employee ?
+                    {!auth.user ?
                             <NavLink className="link" to='/login'>Login</NavLink> :
                             <NavLink className="link" to='/' onClick={async (event) => {
                                 event.preventDefault();
