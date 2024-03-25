90c9802b9dfa3b251037c45bacf4dbd845c0843a
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-03-25 01:36:36 -0500

    added more debug statements

--- server/authenticateToken.js
+++ server/authenticateToken.js
@@ -7,6 +7,13 @@ async function authenticateToken(req, res, next) {
   console.log("entering authentiationToken...")
   try {
     const token = req.headers.cookie?.split('; ').find(row => row.startsWith('token='))?.split('=')[1];
+    console.log(req);
+    console.log(req.headers);
+    console.log(req.headers.cookie);
+    console.log(req.headers.cookie?.split('; '));
+    console.log(req.headers.cookie?.split('; ').find(row => row.startsWith('token=')));
+    console.log(req.headers.cookie?.split('; ').find(row => row.startsWith('token=')))?.split('=');
+    console.log(req.headers.cookie?.split('; ').find(row => row.startsWith('token=')))?.split('=')[1];
     if (!token) {
       console.log("entering NOT token...")
       // throw new Error('No token provided');
