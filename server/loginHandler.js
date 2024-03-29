9d546acced5f32c205d1299b83587f8e11a920fa
2216000292 <z2216000292@gmail.com>
2024-03-29 01:07:04 -0500

    fix login errer and ad encryption on password

--- server/loginHandler.js
+++ server/loginHandler.js
@@ -1,27 +1,37 @@
 // loginHandler.js
 const pool = require('./database');
+const bcrypt = require('bcrypt');
 const getPostData = require('./postDataParser');
 const jwt = require('jsonwebtoken');
+
 
 async function loginHandler(req, res) {
     try {
         const { email, password } = await getPostData(req);
-        console.log("(login handler)receieve ",email, password);
-        const query = 'SELECT * FROM user WHERE UEmail = ? AND UPassword = ?';
-        const [users] = await pool.execute(query, [email, password]);
+        const query = 'SELECT * FROM user WHERE UEmail = ?';
+        const [users] = await pool.execute(query, [email]);
         const user = users[0];
 
         if (user) {
-            const token = jwt.sign({ userId: user.UserID, userType: 'user' }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            console.log('token is initiated in loginHandler.js : ', token);
-            console.log('user is initiated in loginHandler.js : ', user);
-            res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/; SameSite=None; Secure`);
-            // res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/;`);
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Login successful', user, token }));
+            const isPasswordValid = await bcrypt.compare(password, user.UPassword);
+            if (isPasswordValid) {
+                res.setHeader('Set-Cookie', 'token=; HttpOnly; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+                const token = jwt.sign({ userId: user.UserID, userType: 'user' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                console.log('token is initiated in loginHandler.js : ', token);
+                console.log('user is initiated in loginHandler.js : ', user);
+                const isProduction = process.env.NODE_ENV === 'production';
+                const cookieSettings = `token=${token}; HttpOnly; Path=/;` + (isProduction ? ' SameSite=None; Secure' : '');
+                res.setHeader('Set-Cookie', cookieSettings);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Login successful', user, token }));
+            } else {
+                res.writeHead(401, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Password is incorrect' }));
+            }
+
         } else {
             res.writeHead(401, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Email or password is incorrect' }));
+            res.end(JSON.stringify({ message: 'invalid email address' }));
         }
     } catch (error) {
         console.error(error);
@@ -31,3 +41,4 @@ async function loginHandler(req, res) {
 }
 
 module.exports = loginHandler;
+
--- server/signupHandler.js
+++ server/signupHandler.js
@@ -1,24 +1,27 @@
 // signupHandler.js
+const bcrypt = require('bcrypt');
 const pool = require('./database');
 const getPostData = require('./postDataParser');
 
 async function signupHandler(req, res) {
-    try {
-      const { username, fullname, email, password } = await getPostData(req);
+  try {
+    const { username, fullname, email, password } = await getPostData(req);
 
-      const [result] = await pool.execute(
-        'INSERT INTO user (UUsername, UName, UEmail, UPassword) VALUES (?,?,?,?)',
-        [username, fullname, email, password]
-      );
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-      res.writeHead(201, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'User created successfully' }));
-    } catch (error) {
-      console.error(error);
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Error creating user', error: error.toString() }));
-    }
+    const [result] = await pool.execute(
+      'INSERT INTO user (UUsername, UName, UEmail, UPassword) VALUES (?,?,?,?)',
+      [username, fullname, email, hashedPassword]
+    );
+
+    res.writeHead(201, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User created successfully' }));
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Error creating user', error: error.toString() }));
   }
-
+}
 
 module.exports = signupHandler;
